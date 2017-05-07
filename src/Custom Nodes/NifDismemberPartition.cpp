52952440652a5ee11958059b1c0b3e53eceb0b71
Alecu <alecu89@yahoo.com>
2017-05-07 19:28:22 +0300

    fixed dismember flags

--- src/Custom Nodes/NifDismemberPartition.cpp
+++ src/Custom Nodes/NifDismemberPartition.cpp
@@ -324,6 +324,198 @@ MStringArray NifDismemberPartition::bodyPartTypeToStringArray( BSDismemberBodyPa
 			body_parts = (BSDismemberBodyPartType)(body_parts & ~32);
 			is_done = false;
 		}
+		else if ((SBP_30_HEAD & body_parts) == SBP_30_HEAD) {
+			ret.append("SBP_30_HEAD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_30_HEAD);
+			is_done = false;
+		}
+		else if ((SBP_31_HAIR & body_parts) == SBP_31_HAIR) {
+			ret.append("SBP_31_HAIR");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_31_HAIR);
+			is_done = false;
+		}
+		else if ((SBP_32_BODY & body_parts) == SBP_32_BODY) {
+			ret.append("SBP_32_BODY");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_32_BODY);
+			is_done = false;
+		}
+		else if ((SBP_33_HANDS & body_parts) == SBP_33_HANDS) {
+			ret.append("SBP_33_HANDS");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_33_HANDS);
+			is_done = false;
+		}
+		else if ((SBP_34_FOREARMS & body_parts) == SBP_34_FOREARMS) {
+			ret.append("SBP_34_FOREARMS");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_34_FOREARMS);
+			is_done = false;
+		}
+		else if ((SBP_35_AMULET & body_parts) == SBP_35_AMULET) {
+			ret.append("SBP_35_AMULET");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_35_AMULET);
+			is_done = false;
+		}
+		else if ((SBP_36_RING & body_parts) == SBP_36_RING) {
+			ret.append("SBP_36_RING");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_36_RING);
+			is_done = false;
+		}
+		else if ((SBP_37_FEET & body_parts) == SBP_37_FEET) {
+			ret.append("SBP_37_FEET");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_37_FEET);
+			is_done = false;
+		}
+		else if ((SBP_38_CALVES & body_parts) == SBP_38_CALVES) {
+			ret.append("SBP_38_CALVES");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_38_CALVES);
+			is_done = false;
+		}
+		else if ((SBP_39_SHIELD & body_parts) == SBP_39_SHIELD) {
+			ret.append("SBP_39_SHIELD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_39_SHIELD);
+			is_done = false;
+		}
+		else if ((SBP_40_TAIL & body_parts) == SBP_40_TAIL) {
+			ret.append("SBP_40_TAIL");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_40_TAIL);
+			is_done = false;
+		}
+		else if ((SBP_41_LONGHAIR & body_parts) == SBP_41_LONGHAIR) {
+			ret.append("SBP_41_LONGHAIR");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_41_LONGHAIR);
+			is_done = false;
+		}
+		else if ((SBP_42_CIRCLET & body_parts) == SBP_42_CIRCLET) {
+			ret.append("SBP_42_CIRCLET");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_42_CIRCLET);
+			is_done = false;
+		}
+		else if ((SBP_43_EARS & body_parts) == SBP_43_EARS) {
+			ret.append("SBP_43_EARS");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_43_EARS);
+			is_done = false;
+		}
+		else if ((SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH & body_parts) == SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH) {
+			ret.append("SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH);
+			is_done = false;
+		}
+		else if ((SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK & body_parts) == SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK) {
+			ret.append("SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK);
+			is_done = false;
+		}
+		else if ((SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY & body_parts) == SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY) {
+			ret.append("SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY);
+			is_done = false;
+		}
+		else if ((SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK & body_parts) == SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK) {
+			ret.append("SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK);
+			is_done = false;
+		}
+		else if ((SBP_48_MOD_MISC1 & body_parts) == SBP_48_MOD_MISC1) {
+			ret.append("SBP_48_MOD_MISC1");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_48_MOD_MISC1);
+			is_done = false;
+		}
+		else if ((SBP_49_MOD_PELVIS_PRIMARY & body_parts) == SBP_49_MOD_PELVIS_PRIMARY) {
+			ret.append("SBP_49_MOD_PELVIS_PRIMARY");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_49_MOD_PELVIS_PRIMARY);
+			is_done = false;
+		}
+		else if ((SBP_50_DECAPITATEDHEAD & body_parts) == SBP_50_DECAPITATEDHEAD) {
+			ret.append("SBP_50_DECAPITATEDHEAD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_50_DECAPITATEDHEAD);
+			is_done = false;
+		}
+		else if ((SBP_51_DECAPITATE & body_parts) == SBP_51_DECAPITATE) {
+			ret.append("SBP_51_DECAPITATE");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_51_DECAPITATE);
+			is_done = false;
+		}
+		else if ((SBP_52_MOD_PELVIS_SECONDARY & body_parts) == SBP_52_MOD_PELVIS_SECONDARY) {
+			ret.append("SBP_52_MOD_PELVIS_SECONDARY");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_52_MOD_PELVIS_SECONDARY);
+			is_done = false;
+		}
+		else if ((SBP_53_MOD_LEG_RIGHT & body_parts) == SBP_53_MOD_LEG_RIGHT) {
+			ret.append("SBP_53_MOD_LEG_RIGHT");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_53_MOD_LEG_RIGHT);
+			is_done = false;
+		}
+		else if ((32 & body_parts) == SBP_54_MOD_LEG_LEFT) {
+			ret.append("SBP_54_MOD_LEG_LEFT");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_54_MOD_LEG_LEFT);
+			is_done = false;
+		}
+		else if ((SBP_55_MOD_FACE_JEWELRY & body_parts) == SBP_55_MOD_FACE_JEWELRY) {
+			ret.append("SBP_55_MOD_FACE_JEWELRY");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_55_MOD_FACE_JEWELRY);
+			is_done = false;
+		}
+		else if ((SBP_57_MOD_SHOULDER & body_parts) == SBP_57_MOD_SHOULDER) {
+			ret.append("SBP_57_MOD_SHOULDER");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_57_MOD_SHOULDER);
+			is_done = false;
+		}
+		else if ((SBP_58_MOD_ARM_LEFT & body_parts) == SBP_58_MOD_ARM_LEFT) {
+			ret.append("SBP_58_MOD_ARM_LEFT");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_58_MOD_ARM_LEFT);
+			is_done = false;
+		}
+		else if ((SBP_59_MOD_ARM_RIGHT & body_parts) == SBP_59_MOD_ARM_RIGHT) {
+			ret.append("SBP_59_MOD_ARM_RIGHT");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_59_MOD_ARM_RIGHT);
+			is_done = false;
+		}
+		else if ((SBP_60_MOD_MISC2 & body_parts) == SBP_60_MOD_MISC2) {
+			ret.append("SBP_60_MOD_MISC2");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_60_MOD_MISC2);
+			is_done = false;
+		}
+		else if ((SBP_61_FX01 & body_parts) == SBP_61_FX01) {
+			ret.append("SBP_61_FX01");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_61_FX01);
+			is_done = false;
+		}
+		else if ((SBP_130_HEAD & body_parts) == SBP_130_HEAD) {
+			ret.append("SBP_130_HEAD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_130_HEAD);
+			is_done = false;
+		}
+		else if ((SBP_131_HAIR & body_parts) == SBP_131_HAIR) {
+			ret.append("SBP_131_HAIR");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_131_HAIR);
+			is_done = false;
+		}
+		else if ((SBP_141_LONGHAIR & body_parts) == SBP_141_LONGHAIR) {
+			ret.append("SBP_141_LONGHAIR");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_141_LONGHAIR);
+			is_done = false;
+		}
+		else if ((SBP_142_CIRCLET & body_parts) == SBP_142_CIRCLET) {
+			ret.append("SBP_142_CIRCLET");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_142_CIRCLET);
+			is_done = false;
+		}
+		else if ((SBP_143_EARS & body_parts) == SBP_143_EARS) {
+			ret.append("SBP_143_EARS");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_143_EARS);
+			is_done = false;
+		}
+		else if ((SBP_150_DECAPITATEDHEAD & body_parts) == SBP_150_DECAPITATEDHEAD) {
+			ret.append("SBP_150_DECAPITATEDHEAD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_150_DECAPITATEDHEAD);
+			is_done = false;
+		}
+		else if ((SBP_230_HEAD & body_parts) == SBP_230_HEAD) {
+			ret.append("SBP_230_HEAD");
+			body_parts = (BSDismemberBodyPartType)(body_parts & ~SBP_230_HEAD);
+			is_done = false;
+		}
+
+		
 	}
 
 	return ret;
@@ -497,6 +689,162 @@ BSDismemberBodyPartType NifDismemberPartition::stringArrayToBodyPartType( const 
 			ret = (BSDismemberBodyPartType)(ret | BP_TORSOSECTION_BRAIN);
 		} else if(current_flag == "UNKNOWN_IMPORTANT_FLAG_32") {
 			ret = (BSDismemberBodyPartType)(ret | 32);
+		}
+		else if (current_flag == "SBP_30_HEAD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_30_HEAD);
+		}
+		else if (current_flag == "SBP_31_HAIR") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_31_HAIR);
+		}
+		else if (current_flag == "SBP_32_BODY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_32_BODY);
+		}
+		else if (current_flag == "SBP_33_HANDS") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_33_HANDS);
+		}
+		else if (current_flag == "SBP_34_FOREARMS") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_34_FOREARMS);
+		}
+		else if (current_flag == "SBP_35_AMULET") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_35_AMULET);
+		}
+		else if (current_flag == "SBP_36_RING") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_36_RING);
+		}
+		else if (current_flag == "SBP_37_FEET") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_37_FEET);
+		}
+		else if (current_flag == "SBP_38_CALVES") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_38_CALVES);
+		}
+		else if (current_flag == "SBP_39_SHIELD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_39_SHIELD);
+		}
+		else if (current_flag == "SBP_40_TAIL") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_40_TAIL);
+		}
+		else if (current_flag == "SBP_41_LONGHAIR") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_41_LONGHAIR);
+		}
+		else if (current_flag == "SBP_42_CIRCLET") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_42_CIRCLET);
+		}
+		else if (current_flag == "SBP_43_EARS") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_43_EARS);
+		}
+		else if (current_flag == "SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_44_DRAGON_BLOODHEAD_OR_MOD_MOUTH);
+		}
+		else if (current_flag == "SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_45_DRAGON_BLOODWINGL_OR_MOD_NECK);
+		}
+		else if (current_flag == "SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_46_DRAGON_BLOODWINGR_OR_MOD_CHEST_PRIMARY);
+		}
+		else if (current_flag == "SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_47_DRAGON_BLOODTAIL_OR_MOD_BACK);
+		}
+		else if (current_flag == "SBP_48_MOD_MISC1") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_48_MOD_MISC1);
+		}
+		else if (current_flag == "SBP_49_MOD_PELVIS_PRIMARY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_49_MOD_PELVIS_PRIMARY);
+		}
+		else if (current_flag == "SBP_50_DECAPITATEDHEAD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_50_DECAPITATEDHEAD);
+		}
+		else if (current_flag == "SBP_51_DECAPITATE") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_51_DECAPITATE);
+		}
+		else if (current_flag == "SBP_52_MOD_PELVIS_SECONDARY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_52_MOD_PELVIS_SECONDARY);
+		}
+		else if (current_flag == "SBP_53_MOD_LEG_RIGHT") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_53_MOD_LEG_RIGHT);
+		}
+		else if (current_flag == "SBP_54_MOD_LEG_LEFT") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_54_MOD_LEG_LEFT);
+		}
+		else if (current_flag == "SBP_55_MOD_FACE_JEWELRY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_55_MOD_FACE_JEWELRY);
+		}
+		else if (current_flag == "SBP_56_MOD_CHEST_SECONDARY") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_56_MOD_CHEST_SECONDARY);
+		}
+		else if (current_flag == "SBP_57_MOD_SHOULDER") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_57_MOD_SHOULDER);
+		}
+		else if (current_flag == "SBP_58_MOD_ARM_LEFT") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_58_MOD_ARM_LEFT);
+		}
+		else if (current_flag == "SBP_59_MOD_ARM_RIGHT") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_59_MOD_ARM_RIGHT);
+		}
+		else if (current_flag == "SBP_60_MOD_MISC2") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_60_MOD_MISC2);
+		}
+		else if (current_flag == "SBP_61_FX01") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_61_FX01);
+		}
+		else if (current_flag == "SBP_130_HEAD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_130_HEAD);
+		}
+		else if (current_flag == "SBP_131_HAIR") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_131_HAIR);
+		}
+		else if (current_flag == "SBP_141_LONGHAIR") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_141_LONGHAIR);
+		}
+		else if (current_flag == "SBP_142_CIRCLET") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_142_CIRCLET);
+		}
+		else if (current_flag == "SBP_143_EARS") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_143_EARS);
+		}
+		else if (current_flag == "SBP_150_DECAPITATEDHEAD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_150_DECAPITATEDHEAD);
+		}
+		else if (current_flag == "SBP_230_HEAD") {
+			/*!< Torso Section | Brain */
+			ret = (BSDismemberBodyPartType)(ret | SBP_230_HEAD);
 		}
 	}
 
